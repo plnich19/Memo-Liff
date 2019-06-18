0f0f7acdb373aaee66d269540fdb2a34e4362fb4
Nichakan Udomlerkchai <PloyNichakan@gmail.com>
2019-06-18 14:20:50 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,24 @@ import Header from '../src/components/Header';
 import MessageList from './components/MessageList';
 import MessageBox from './components/MessageBox';
 import firebase from 'firebase';
+
+const liff = window.liff;  
 class App extends Component {
-  constructor(props){super(props);var config = {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      groupId: '',
+      displayName : '',
+      userId : '',
+      pictureUrl : '',
+      statusMessage : ''
+    };
+
+    this.initialize = this.initialize.bind(this);
+    this.closeApp = this.closeApp.bind(this);
+
+    var config = {
     apiKey: "AIzaSyAFXBsagqOOXzWySieOoYNDs8HaTUuRk6k",
     authDomain: "testproject-6e25f.firebaseapp.com",
     databaseURL: "https://testproject-6e25f.firebaseio.com/",
@@ -15,6 +31,35 @@ class App extends Component {
   };
   firebase.initializeApp(config);
 }
+
+componentDidMount() {
+  window.addEventListener('load', this.initialize);
+}
+
+initialize() {
+  liff.init(async (data) => {
+    let groupId = data.context.groupId;
+    console.log(groupId)
+    let profile = await liff.getProfile();
+    this.setState({
+      displayName : profile.displayName,
+      userId : profile.userId,
+      pictureUrl : profile.pictureUrl,
+      statusMessage : profile.statusMessage
+    });
+  }); 
+}
+
+closeApp(event) {
+  event.preventDefault();
+  liff.sendMessages([{
+    type: 'text',
+    text: "Thank you, Bye!"
+  }]).then(() => {
+    liff.closeWindow();
+  });
+}
+
 render() {
   return (
     <div className="container">
@@ -31,6 +76,7 @@ render() {
           <MessageBox db={firebase} />
         </div>
       </div>
+      <Button color="primary" onClick={this.closeApp}>Close</Button>
     </div>
   );
  }
