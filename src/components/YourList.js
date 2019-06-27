cb2730e5188b512e819b7e9a2fb386f7af76ee5e
nutchaphon <nutchaphon.eiei@gmail.com>
2019-06-27 17:57:22 +0700

    update

--- src/components/YourList.js
+++ src/components/YourList.js
@@ -22,13 +22,15 @@ class YourList extends Component {
             openDelete: false,
             checked: false,
             openCheck: false,
-            groupId: ''
+            groupId:''
         };
 
     }
 
 
     liffSuccess = (data) => {
+        console.log(data)
+        const groupId = data.context.groupId;
         let profile = liff.getProfile();
         this.setState({
             liffData: data,
@@ -36,83 +38,83 @@ class YourList extends Component {
             userId: profile.userId,
             pictureUrl: profile.pictureUrl,
             statusMessage: profile.statusMessage,
-            groupId: groupId
-        });
+        groupId: groupId 
+    });
+}
+
+liffError = (err) => {
+    console.error("liffError")
+    this.setState({
+        liffData: err
+    });
+}
+
+
+initialize = () => {
+    liff.init(this.liffSuccess, this.liffError)
+}
+
+componentDidMount() {
+    window.addEventListener('load', this.initialize);
+    // let instantLists = []
+    // const ref = firebase.firestore().collection('data').doc('groupId-fadgeagsdfreasdgfgesdf').collection('tasks');
+    // const query = ref.where('assignee', 'array-contains', this.state.displayName).get()
+    //     .then(snapshot => {
+    //         snapshot.forEach(doc => {
+    //             instantLists.push(doc.id)
+    //         });
+    //         console.log(instantLists, 'instantLists')
+    //         this.setState({
+    //             getYourList: instantLists
+    //         })
+    //     })
+    //     .catch(err => {
+    //         console.log('Error getting documents', err);
+    //     });
+}
+
+onOpenEditModal = () => {
+    this.setState({ openEdit: true });
+};
+
+onCloseEditModal = () => {
+    this.setState({ openEdit: false });
+};
+
+onOpenDeleteModal = () => {
+    this.setState({ openDelete: true });
+};
+
+onCloseDeleteModal = () => {
+    this.setState({ openDelete: false });
+};
+
+onOpenCheckModal = () => {
+    this.setState({ openCheck: true });
+};
+
+onCloseCheckModal = () => {
+    this.setState({ openCheck: false });
+};
+
+handleCheckboxChange = event =>
+    this.setState({ checked: event.target.checked });
+
+changeCheck = () => {
+    if (this.state.checked === false) {
+        this.setState({ checked: true, openCheck: false })
     }
-
-    liffError = (err) => {
-        console.error("liffError")
-        this.setState({
-            liffData: err
-        });
+    if (this.state.checked === true) {
+        this.setState({ checked: false, openCheck: false })
     }
+}
 
+render() {
+    const { liffData } = this.state
+    return (
+        <div>
 
-    initialize = () => {
-        liff.init(this.liffSuccess, this.liffError)
-    }
-
-    componentDidMount() {
-        window.addEventListener('load', this.initialize);
-        // let instantLists = []
-        // const ref = firebase.firestore().collection('data').doc('groupId-fadgeagsdfreasdgfgesdf').collection('tasks');
-        // const query = ref.where('assignee', 'array-contains', this.state.displayName).get()
-        //     .then(snapshot => {
-        //         snapshot.forEach(doc => {
-        //             instantLists.push(doc.id)
-        //         });
-        //         console.log(instantLists, 'instantLists')
-        //         this.setState({
-        //             getYourList: instantLists
-        //         })
-        //     })
-        //     .catch(err => {
-        //         console.log('Error getting documents', err);
-        //     });
-    }
-
-    onOpenEditModal = () => {
-        this.setState({ openEdit: true });
-    };
-
-    onCloseEditModal = () => {
-        this.setState({ openEdit: false });
-    };
-
-    onOpenDeleteModal = () => {
-        this.setState({ openDelete: true });
-    };
-
-    onCloseDeleteModal = () => {
-        this.setState({ openDelete: false });
-    };
-
-    onOpenCheckModal = () => {
-        this.setState({ openCheck: true });
-    };
-
-    onCloseCheckModal = () => {
-        this.setState({ openCheck: false });
-    };
-
-    handleCheckboxChange = event =>
-        this.setState({ checked: event.target.checked });
-
-    changeCheck = () => {
-        if (this.state.checked === false) {
-            this.setState({ checked: true, openCheck: false })
-        }
-        if (this.state.checked === true) {
-            this.setState({ checked: false, openCheck: false })
-        }
-    }
-
-    render() {
-        const { liffData } = this.state
-        return (
-            <div>
-
-                {/* <div>
+            {/* <div>
                     <h1>Your Tasks</h1>
                     <table className='alllisttable'>
                         {
@@ -124,52 +126,52 @@ class YourList extends Component {
                         }
                     </table>
                 </div> */}
-                <p>17:47</p>
-                <p>{liffData}</p>
-                <p>{this.state.displayName}</p>
-                <button className='editModal' onClick={this.onOpenEditModal}>Edit</button>
-                <Modal open={this.state.openEdit} onClose={this.onCloseEditModal} center>
-                    <h2>Edit</h2>
-                    <form>
-                        <p>Tasks: </p>
-                        <input type="text" />
-                        <p>Responsibility: </p>
-                        <input type="text" />
-                        <div></div>
-                    </form>
-                    <button>Update</button>
-                    <button onClick={this.onCloseEditModal}>Cancel</button>
-                </Modal>
+            <p>17:56</p>
+            <p>{liffData}</p>
+            <p>{this.state.displayName}</p>
+            <button className='editModal' onClick={this.onOpenEditModal}>Edit</button>
+            <Modal open={this.state.openEdit} onClose={this.onCloseEditModal} center>
+                <h2>Edit</h2>
+                <form>
+                    <p>Tasks: </p>
+                    <input type="text" />
+                    <p>Responsibility: </p>
+                    <input type="text" />
+                    <div></div>
+                </form>
+                <button>Update</button>
+                <button onClick={this.onCloseEditModal}>Cancel</button>
+            </Modal>
 
-                <button className='deleteModal' onClick={this.onOpenDeleteModal}>Delete</button>
-                <Modal open={this.state.openDelete} onClose={this.onCloseDeleteModal} center>
-                    <h2>Delete!!!</h2>
-                    <div>
-                        <p className='deleteText'>Are you sure you want to delete this task?</p>
+            <button className='deleteModal' onClick={this.onOpenDeleteModal}>Delete</button>
+            <Modal open={this.state.openDelete} onClose={this.onCloseDeleteModal} center>
+                <h2>Delete!!!</h2>
+                <div>
+                    <p className='deleteText'>Are you sure you want to delete this task?</p>
 
-                        <button>Delete</button>
-                        <button onClick={this.onCloseDeleteModal}>Cancel</button>
-                    </div>
-                </Modal>
+                    <button>Delete</button>
+                    <button onClick={this.onCloseDeleteModal}>Cancel</button>
+                </div>
+            </Modal>
 
-                <label className='checkboxContainer'>
-                    <span className='checkboxText'>Done?</span>
-                    <input type='checkbox' className='checkDone'
-                        checked={this.state.checked}
-                        onClick={this.onOpenCheckModal}
-                    // onChange={this.handleCheckboxChange}
-                    ></input>
-                </label>
-                <Modal open={this.state.openCheck} onClose={this.onCloseCheckModal} center>
-                    <h2>Status</h2>
-                    <p className='checkText'>Are you sure you really done this task?</p>
-                    <div>
-                        <button onClick={this.changeCheck}>Yes</button>
-                        <button onClick={this.onCloseCheckModal}>No</button>
-                    </div>
-                </Modal>
-            </div>
-        );
-    }
+            <label className='checkboxContainer'>
+                <span className='checkboxText'>Done?</span>
+                <input type='checkbox' className='checkDone'
+                    checked={this.state.checked}
+                    onClick={this.onOpenCheckModal}
+                // onChange={this.handleCheckboxChange}
+                ></input>
+            </label>
+            <Modal open={this.state.openCheck} onClose={this.onCloseCheckModal} center>
+                <h2>Status</h2>
+                <p className='checkText'>Are you sure you really done this task?</p>
+                <div>
+                    <button onClick={this.changeCheck}>Yes</button>
+                    <button onClick={this.onCloseCheckModal}>No</button>
+                </div>
+            </Modal>
+        </div>
+    );
+}
 }
 export default YourList;
